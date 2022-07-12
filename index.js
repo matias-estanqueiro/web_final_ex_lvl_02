83bbc5b6ed57ce87a5f06f23b011b697e866281b
matias-estanqueiro <matias.estanqueiro@gmail.com>
2022-07-12 16:16:13 -0300

    add_npm_cors

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ require("dotenv").config();
 require("./data/config");
 const port = process.env.PORT;
 
+const cors = require("cors");
+
 const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
